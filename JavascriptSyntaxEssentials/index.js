4cda2018780b66fac5f72ec94351560467f3022d
andrew <andrewchavez@mail.com>
2024-10-29 03:36:43 +0000

    created basic js logs to demonstrate outputs

---
+++ JavascriptSyntaxEssentials/index.js
@@ -0,0 +1,22 @@
+// This JavaScript file demonstrates basic syntax, including comments, console.log, and math operations.
+
+//addition of 10 and 5
+console.log(10+5);
+
+//subtraction of 10 from 20
+console.log(20-10);
+
+//multiplication of 4 and 5
+console.log(4*5);
+
+//division of 20 by 4
+console.log(20/4);
+
+//Print a greeting message as a string
+console.log('Hello, my name is Andrew!');
+
+//Combine a string and a number
+console.log('The number is '+42);
+
+//Print the sum of 5 and 3 with a decriptive message
+console.log('The sum of 5 and 3 is '+ (5+3));
